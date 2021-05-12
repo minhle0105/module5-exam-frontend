b47fe5405901841cdea96237ddb9834d7acfe5fb
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-05-12 13:45:50 +0700

    Them sach moi

--- src/app/book/create-book/create-book.component.ts
+++ src/app/book/create-book/create-book.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Book} from '../../model/book';
+import {BookService} from '../../service/book.service';
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-create-book',
   templateUrl: './create-book.component.html',
   styleUrls: ['./create-book.component.css']
 })
 export class CreateBookComponent implements OnInit {
-
-  constructor() { }
+  book: Book = {};
+  constructor(private bookService: BookService) { }
 
   ngOnInit() {
   }
+
+  addNewBook(form: NgForm) {
+    let newBook = form.value;
+    this.bookService.createNewBook(newBook).subscribe(() => {
+      alert("Successfully added");
+      this.book = {};
+    }, e => {
+      console.log(e);
+      alert("Fail to add")
+    });
+  }
+
+
 
 }
