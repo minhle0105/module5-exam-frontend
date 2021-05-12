afc2e3fdd1a97211d3852ae44f14277b66864b59
minhle0105 <minh.anh.le.cnn@gmail.com>
2021-05-12 14:34:16 +0700

    Show thong tin sach duoi dang form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import {BookListComponent} from './book/book-list/book-list.component';
 import {CreateBookComponent} from './book/create-book/create-book.component';
 import {UpdateBookComponent} from './book/update-book/update-book.component';
+import {BookDetailsComponent} from './book/book-details/book-details.component';
 
 
 const routes: Routes = [
   {
     path: 'books',
     component: BookListComponent
+  },
+  {
+    path: 'books/:id',
+    component: BookDetailsComponent
   },
   {
     path: 'books/create',
--- src/app/book/book-details/book-details.component.ts
+++ src/app/book/book-details/book-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Book} from '../../model/book';
+import {BookService} from '../../service/book.service';
+import {FormControl, FormGroup} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent implements OnInit {
-
-  constructor() { }
+  book: Book = {};
+  currentId: number = -1;
+  bookForm = new FormGroup( {
+    title: new FormControl(),
+    author: new FormControl(),
+    description: new FormControl()
+  })
+  constructor(private bookService: BookService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      this.currentId = +paramMap.get('id');
+      this.getBookById(this.currentId);
+    })
+  }
 
   ngOnInit() {
+  }
+
+  get id() {
+    return this.bookForm.get('id');
+  }
+
+  getBookById(id: number) {
+    this.bookService.getBookById(id).subscribe(thisbook => {
+      this.bookForm = new FormGroup({
+        title: new FormControl(thisbook.title),
+        author: new FormControl(thisbook.author),
+        description: new FormControl(thisbook.description)
+      })
+    })
   }
 
 }
